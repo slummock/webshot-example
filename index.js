7546cabaf38b700e155ce5197025e0b110e258df
slummock <slummock@gmail.com>
2019-02-19 21:32:57 +0000

    ignoring output, added selector example

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ webshot = require('webshot');
 request = require('request');
 fs = require('fs');
 
-var readurl = "https://google.com/";
-var filepath = './temp.png'
+var readurl = "https://github.com/slummock/webshot-example";
+var filepath = './output/temp.png'
 var options = {
-    captureSelector: ".#lga"
+    captureSelector: ".repository-content ",
+    
+    userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36"
 };
 
 webshot(readurl, filepath, options, function (err) {
+    console.log(err);
 });
